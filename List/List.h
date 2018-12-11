be0a0714d2bb7fc68f47d9be33c0b60f27c54bd3
liyulong6 <18709202005@163.com>
2018-12-11 18:11:37 +0800

    20181211

--- List/List.h
+++ List/List.h
@@ -5,118 +5,186 @@ using namespace std;
 
 namespace bit
 {
-    template<class T>
+template<class T>
     struct list_node
+{
+public:
+    list_node(const T& value = T())
+        : pre(nullptr)
+          , next(nullptr)
+          , val(value)
+    {}
+
+    list_node<T>* pre;
+    list_node<T>* next;
+    T val;
+};
+
+template<class T>
+struct list_iterator
+{
+    typedef list_iterator<T> self;
+
+    list_iterator(list_node<T>* pNode = nullptr)
+        :_pNode(pNode)
+    {}
+
+    list_iterator(const self& s)
+        :_pNode(s._pNode)
+    {}
+
+
+    T& operator*()
     {
-        public:
-            list_node(const T& value = T())
-                : pre(nullptr)
-                , next(nullptr)
-                , val(value)
-        {}
+        return _pNode->val;
+    }
 
-            list_node<T>* pre;
-            list_node<T>* next;
-            T val;
-    };
 
-    template<class T>
-    struct list_iterator
+    T* operator->()
     {
-        typedef list_iterator<T> self;
-
-        list_iterator(list_node<T>* pNode = nullptr)
-            :_pNode(pNode)
-        {}
-
-        list_iterator(const self& s)
-            :_pNode(s._pNode)
-        {}
-
-        
-        T& operator*()
-        {
-            return _pNode->val;
-        }
+        return (&(operator*()));
+    }
 
 
-        T* operator->()
-        {
-            return (&(operator*()));
-        }
-
-
-        self& operator++()
-        {
-            _pNode = _pNode->next;
-            return *this;
-        }
-
-        self operator++(int)
-        {
-            self stemp(*this);
-            _pNode = _pNode->next;
-
-            return stemp;
-        }
-
-
-        self& operator--()
-        {
-            _pNode = _pNode->pre;
-            return *this;
-        }
-
-        self operator--(int)
-        {
-            self stemp(*this);
-            _pNode = _pNode->pre;
-
-            return stemp;
-        }
-
-
-        bool operator!=(const self& s)
-        {
-            return _pNode != s._pNode;
-        }
-
-
-        
-        bool operator ==(const self& s)
-        {
-            return _pNode == s._pNode;
-        }
-
-
-
-        list_node<T>* _pNode;
-    };
-
-
-    template<class T>
-    class list
+    self& operator++()
     {
-        typedef list_node<T> node;
-        typedef node* pNode;
-    public:
-        typedef list_iterator<T>  iterator;
-        typedef const list_iterator<T>  const_iterator;
-    public:
-        list()
-        {
-            createhead();
-            _pHead->next = _pHead->pre = _pHead;
-        }
+        _pNode = _pNode->next;
+        return *this;
+    }
 
-        list(int n, const T& val)
-        {
-            createhead();
-            for(size_t i = 0;i < n; ++i)
-                push_back(val);
-        }
+    self operator++(int)
+    {
+        self stemp(*this);
+        _pNode = _pNode->next;
 
-        template<class Input_iterator>
+        return stemp;
+    }
+
+
+    self& operator--()
+    {
+        _pNode = _pNode->pre;
+        return *this;
+    }
+
+    self operator--(int)
+    {
+        self stemp(*this);
+        _pNode = _pNode->pre;
+
+        return stemp;
+    }
+
+    bool operator!=(const self& s)
+    {
+        return _pNode != s._pNode;
+    }
+
+
+
+    bool operator ==(const self& s)
+    {
+        return _pNode == s._pNode;
+    }
+
+
+
+    list_node<T>* _pNode;
+};
+
+template<class T>
+class reverse_list_iterator
+{
+    typedef list_node<T> node;
+    typedef node* PNode;
+    typedef reverse_list_iterator<T> self;
+public:
+    reverse_list_iterator(PNode pNode = nullptr)
+        :_pNode(pNode)
+    {}
+
+    reverse_list_iterator(const self& s)
+        :_pNode(s._pNode)
+    {}
+
+    T& operator*()
+    {
+        PNode* pNode = _pNode;
+        pNode = pNode->pre;
+        return pNode->val;
+    }
+
+    T* operator->()
+    {
+        return &(operator*());
+    }
+
+    self& operator++()
+    {
+        _pNode = _pNode->pre;
+        return *this;
+    }
+
+    self operator++(int)
+    {
+        self temp(*this);
+        _pNode = _pNode->pre;
+        return temp;
+    }
+
+    self& operator--()
+    {
+        _pNode = _pNode->next;
+        return *this;
+    }
+
+    self operator--(int)
+    {
+        self temp(*this);
+        _pNode = _pNode->next;
+        return temp;
+    }
+
+    bool operator!=(const self& s)
+    {
+        return _pNode != s._pNode;
+    }
+
+
+    bool operator==(const self& s)
+    {
+        return _pNode == s._pNode;
+    }
+private:
+    PNode _pNode;
+
+};
+
+template<class T>
+class list
+{
+    typedef list_node<T> node;
+    typedef node* pNode;
+public:
+    typedef list_iterator<T>  iterator;
+    typedef const list_iterator<T>  const_iterator;
+    typedef reverse_list_iterator<T> reverse_iterator;
+    typedef const reverse_list_iterator<T> const_reverse_iterator;
+public:
+    list()
+    {
+        createhead();
+        _pHead->next = _pHead->pre = _pHead;
+    }
+
+    list(int n, const T& val)
+    {
+        createhead();
+        for(size_t i = 0;i < n; ++i)
+            push_back(val);
+    }
+
+    template<class Input_iterator>
         list(Input_iterator first, Input_iterator last)
         {
             createhead();
@@ -124,22 +192,32 @@ namespace bit
                 push_back(*first++);
         }
 
-        list(const list<T>& l)
-        {
+    list(const list<T>& l)
+    {
+        createhead();
+        for(const auto& e : l)
+            push_back(e);
+    }
 
-        }
-        list<T>& operator=(const list<T>& l);
+
+    list<T>& operator=(const list<T>& l)
+    {
+
+        for(const auto& e : l)
+            push_back(e);
+    }
 
 
-        ~list()
-        {
-            clear();
-            delete _pHead;
-            _pHead = nullptr;
-        }
-/////////////////////////////////////////////////////////////////////
-//iterator
-//
+
+    ~list()
+    {
+        clear();
+        delete _pHead;
+        _pHead = nullptr;
+    }
+    /////////////////////////////////////////////////////////////////////
+    //iterator
+    //
     iterator begin()
     {
         return iterator(_pHead->next);
@@ -154,98 +232,151 @@ namespace bit
 
     iterator end()
     {
-        return iterator(_pHead->pre);
+        return iterator(_pHead);
     }
 
 
     const_iterator end()const
     {
-        return iterator(_pHead->pre);
+        return iterator(_pHead);
     }
-/////////////////////////////////////////////////////////////////////
-//capacity
-        
-        size_t size()const
-        {
-            size_t count = 0;
-            pNode cur = _pHead->next;
-            while(cur != _pHead)
-            {
-                count++;
-                cur = cur->next;
-            }
 
-            return count;
+    reverse_iterator rbegin()
+    {
+        return reverse_iterator(_pHead);
+    }
+
+    const_reverse_iterator rbegin()const
+    {
+        return reverse_iterator(_pHead);
+    }
+
+    reverse_iterator rend()
+    {
+        return reverse_iterator(_pHead->next);
+    }
+
+    const_reverse_iterator rend()const
+    {
+        return reverse_iterator(_pHead->next);
+    }
+    /////////////////////////////////////////////////////////////////////
+    //capacity
+
+    size_t size()const
+    {
+        size_t count = 0;
+        pNode cur = _pHead->next;
+        while(cur != _pHead)
+        {
+            count++;
+            cur = cur->next;
         }
 
+        return count;
+    }
 
-        void resize(size_t n, const T& val = T())
+
+    void resize(size_t n, const T& val = T())
+    {
+        size_t size = this->size();
+        if(n > size)
         {
-            size_t size = this->size();
-            if(n > size)
-            {
-                for(size_t i = size; i < n; ++i)
-                    push_back(val);
-            }
-            else
-            {
-                for(size_t i = n; i < size; ++i)
-                    pop_back();
-            }
+            for(size_t i = size; i < n; ++i)
+                push_back(val);
         }
-
-        bool empty()
+        else
         {
-            return _pHead = _pHead->next;
+            for(size_t i = n; i < size; ++i)
+                pop_back();
         }
-//////////////////////////////////////////////////////////////////////
-//acess
+    }
 
-        T& front()
-        {
-            return _pHead->next->val;
-        }
+    bool empty()
+    {
+        return _pHead = _pHead->next;
+    }
+    //////////////////////////////////////////////////////////////////////
+    //acess
 
-        const T& front()const
-        {
-            return _pHead->next->val;
-        }
+    T& front()
+    {
+        return _pHead->next->val;
+    }
 
-        T& back()
-        {
-            return _pHead->pre->val;
-        }
+    const T& front()const
+    {
+        return _pHead->next->val;
+    }
 
-        const T& back()const
-        {
-            return _pHead->pre->val;
-        }
+    T& back()
+    {
+        return _pHead->pre->val;
+    }
 
-//////////////////////////////////////////////////////////////////////
-//modify
-        void push_back(const T& val)
-        {
-            insert(end(), val);
-        }
+    const T& back()const
+    {
+        return _pHead->pre->val;
+    }
 
-        void pop_back()
-        {
-            erase(--end());
-        }
+    //////////////////////////////////////////////////////////////////////
+    //modify
+    
+    template <class InputIterator>
+    void assign ( InputIterator first, InputIterator last)
+    {
+        clear();
+        insert(end(), first, last);
+    }
+    void assign ( int n, const T& u  )
+    {
+        clear();
+        while(n--)
+            push_back(u);
+    }
 
-        void push_front(const T& val)
-        {
-            insert(begin(), val);
-        }
+    void push_back(const T& val)
+    {
+        insert(_pHead, val);
+    }
 
-        void pop_front()
-        {
-            erase(begin());
-        }
+    void pop_back()
+    {
+        erase(--end());
+    }
 
-        iterator insert(iterator position, const T& val)
+    void push_front(const T& val)
+    {
+        insert(begin(), val);
+    }
+
+    void pop_front()
+    {
+        erase(begin());
+    }
+
+    iterator insert(iterator position, const T& val)
+    {
+        pNode pNewNode = new node(val);
+        pNode pos = position._pNode;
+
+        pNewNode->next = pos;
+        pNewNode->pre = pos->pre;
+
+        pos->pre->next = pNewNode;
+        pos->pre = pNewNode;
+
+        return iterator(pNewNode);
+    }
+
+
+    void insert (iterator position, int n, const T& val)
+    {
+        pNode pNewNode;
+        while(n--)
         {
-            pNode pNewNode = new node(val);
+
+            pNewNode = new node(val);
             pNode pos = position._pNode;
 
             pNewNode->next = pos;
@@ -253,50 +384,117 @@ namespace bit
 
             pos->pre->next = pNewNode;
             pos->pre = pNewNode;
-
-            return iterator(pNewNode);
         }
 
-        iterator erase(iterator position)
+
+    }
+    template <class InputIterator>
+        void insert(iterator position, InputIterator first, InputIterator last)
         {
+            pNode pNewNode;
             pNode pos = position._pNode;
-            if(_pHead == pos)
-                return iterator(_pHead);
-
-            pNode pRet = pos->next;
-            pos->pre->next = pos->next;
-            pos->next->pre = pos->pre;
-
-            delete pos;
-            return iterator(pRet);
-        }
-
-        void swap(list<T>& l)
-        {
-            swap(_pHead, l._pHead);
-        }
-
-        void clear()
-        {
-            //头删
-            pNode cur = _pHead->next;
-            while(cur != _pHead)
+            InputIterator it;
+            for(it = first; it != last; ++it)
             {
-                _pHead->next = cur->next;
-                delete cur;
-                cur = _pHead->next;
-            }
+                pNewNode = new node(*first++);
 
-            _pHead->next = _pHead->pre = _pHead;
+                pNewNode->next = pos;
+                pNewNode->pre = pos->pre;
+                pos->pre->next = pNewNode;
+                pos->pre = pNewNode;
+            }
         }
-    private:
-        void createhead()
+
+
+    iterator erase(iterator position)
+    {
+        pNode pos = position._pNode;
+        if(_pHead == pos)
+            return iterator(_pHead);
+
+        pNode pRet = pos->next;
+        pos->pre->next = pos->next;
+        pos->next->pre = pos->pre;
+
+        delete pos;
+        return iterator(pRet);
+    }
+
+    iterator erase(iterator first, iterator last)
+    {
+        iterator it = first;
+        while(it != last)
         {
-            _pHead = new node();
-            _pHead->next = _pHead->pre = _pHead;
+            iterator temp = it;
+            ++it;
+            erase(temp);
         }
-    private:
-        pNode _pHead;
-    };
+
+    }
+    void swap(list<T>& l)
+    {
+        swap(_pHead, l._pHead);
+
+    }
+    void clear()
+    {
+        //头删
+        pNode cur = _pHead->next;
+        while(cur != _pHead)
+        {
+            _pHead->next = cur->next;
+            delete cur;
+            cur = _pHead->next;
+        }
+
+        _pHead->next = _pHead->pre = _pHead;
+    }
+    ////////////////////////////////////////////////////////////////////////
+    //Operations
+    void splice(iterator position, list<T>& x  )
+    {
+        pNode pos = position._pNode;
+        insert(pos, x.begin(),x.end());
+        x.clear();
+    }
+
+    void splice(iterator position, list<T>& x, iterator i)
+    {
+        pNode pos = position._pNode;
+        insert(pos, *i);
+        x.erase(i);
+    }
+
+    void splice(iterator position, list<T>& x, iterator first, iterator last)
+    {
+        pNode pos = position._pNode;
+        insert(pos, first, last);
+        x.erase(first, last);
+    }
+
+
+    void remove ( const T& value  )
+    {
+        list<int>::iterator it = begin();
+        while(it != end())
+        {
+            list<int>::iterator temp = it;
+            ++it;
+            if(*temp == value)
+                erase(temp);
+
+        }
+    }
+
+
+private:
+    void createhead()
+    {
+        _pHead = new node();
+        _pHead->next = _pHead->pre = _pHead;
+    }
+private:
+    pNode _pHead;
+};
 }
- 
+
--- List/main.cpp
+++ List/main.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
-#include<list>
+#include<vector>
 using namespace std;
 #include"List.h"
 
-template<class T>
+    template<class T>
 void PrintList(const bit::list<T>& l)
 {
     for(const auto e : l)
         cout << e << " ";
     cout << endl;
+
 }
 
 int main()
 {
-    bit::list<int> l1;
-    //bit::list<int> l2(10, 5);
-    //PrintList(l2);
+    bit::list<int> l2(10, 5);
+    PrintList(l2);
 
-    list<int> l{1,2,3,4,5};
+    vector<int> v{1,2,3,4,5,6};
+    bit::list<int> l1(v.begin(), v.end());
+    PrintList(l1);
 
-    bit::list<int> l3(l.begin(),l.end());
-    PrintList(l3);
+    bit::list<int>::iterator it = l1.begin();
+    bit::list<int>::iterator it1 = l1.begin();
+    int n = 3;
+    while(n--)
+        ++it;
+    n = 5;
+    while(n--)
+        ++it1;
+    l2.splice(l2.end(), l1);
+    PrintList(l2);
+    l2.remove(5);
+    PrintList(l2);
     return 0;
 }
 
